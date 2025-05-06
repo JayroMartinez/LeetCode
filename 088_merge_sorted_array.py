9edc3ba715f2df310b279804cc8020cf5033c6e2
Jayro Martínez Cerveró <43171040+JayroMartinez@users.noreply.github.com>
2025-05-06 12:45:13 +0200

    added problem 88: merge sorted array, and changed name to other files

---
+++ 088_merge_sorted_array.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+
+        nums1[m:] = nums2
+        nums1.sort()
+
+
+if __name__ == "__main__":
+    nums1 = [1, 2, 3, 0, 0, 0]
+    m = 3
+    nums2 = [2, 5, 6]
+    n = 3
+
+    Solution().merge(nums1, m, nums2, n)
+    print(nums1)  # Output should be [1, 2, 2, 3, 5, 6]
