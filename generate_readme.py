645cadec4e84e93be12e523a04a809c4e28c2e74
Jayro Martínez Cerveró <43171040+JayroMartinez@users.noreply.github.com>
2025-05-08 17:07:32 +0200

    Modified generate_readme.py

--- generate_readme.py
+++ generate_readme.py
@@ -29,7 +29,7 @@ def extract_info(filename):
     return None
 
 def main():
-    files = [f for f in os.listdir('.') if f.endswith(('.py', '.go'))]
+    files = [f for f in os.listdir('solutions') if f.endswith(('.py', '.go'))]
     problems = []
 
     for f in files:
